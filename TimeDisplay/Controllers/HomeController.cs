fe41df16627e19ec3f072aa7803b95c36b6c4694
spaulding42 <109320147+spaulding42@users.noreply.github.com>
2023-07-24 17:17:19 -0600

    time display

---
+++ TimeDisplay/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TimeDisplay.Models;
+
+namespace TimeDisplay.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = new DateTime(2023, 7, 25);
+        var difOfDates = endTime - currentTime;
+
+        ViewBag.CurrentTime = currentTime;
+        ViewBag.EndTime = endTime;
+        ViewBag.DifOfDates = difOfDates;
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
